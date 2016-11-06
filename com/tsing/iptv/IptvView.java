38db41cfb50574954df4ce62eadd41c4b299f504
tsing <tsing@tsing>
2016-11-07 00:02:48 +0800

    added IptvView as the UI

--- com/tsing/iptv/IptvView.java
+++ com/tsing/iptv/IptvView.java
@@ -3,15 +3,17 @@ package com.tsing.iptv;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.text.*;
 
 public class IptvView extends JFrame implements ViewInterface {
   
-  JDialog inputDialog; // to receive input
-  JTextField snField; // to receive SN input
-  JTextField macField; // to receive Mac input
+  public JDialog inputDialog; // to receive input
+  public JTextField snField; // to receive SN input
+  public JTextField macField; // to receive Mac input
 
-  JTextArea infoArea; // to show test infomation
-  JTextArea retArea; // to show xml string return from STB
+  public JTextPane infoArea; // to show test infomation
+  public JTextArea retArea; // to show xml string return from STB
+	public JLabel resultLabel; // to show test result(fail or pass)
 
   MacWriter macWriter;
   XmlParser xmlParser;
@@ -20,6 +22,7 @@ public class IptvView extends JFrame implements ViewInterface {
   XmlWriter xmlWriter;
 
   public IptvView() {
+		/*
     xmlParser = IptvXmlParser(); // initialize xmlParser 
     dbConnector = new DBConnector(); //~~ DBConnector unimplemented yet 
     macWriter = new MacWriter(xmlParser, dbConnector); // initialize macWriter
@@ -29,7 +32,8 @@ public class IptvView extends JFrame implements ViewInterface {
 
     macWriter.addMacWritingListener(this); // register IptvView with macWriter
     macWriter.addMacWritingListener(logger); // register logger with macWriter
-
+		*/
+		
     setUI();
   }
 
@@ -45,17 +49,27 @@ public class IptvView extends JFrame implements ViewInterface {
 
     JPanel panel = new JPanel(new BorderLayout());
 
-    infoArea = new JTextArea();
+    infoArea = new JTextPane();
+		infoArea.setEditable(false);
+		infoArea.setBackground(Color.BLACK);
     infoArea.setBorder(
         BorderFactory.createTitledBorder("Test Information Area"));
     
-    retArea = new JTextArea(12, 40);
+    retArea = new JTextArea(8, 40);
+		retArea.setEditable(false);
+		retArea.setBackground(Color.GRAY);
     retArea.setBorder(
         BorderFactory.createTitledBorder("Ret XML Area"));
     retArea.setLineWrap(true);
     retArea.setVisible(false); // default not show retArea;
 
-    panel.add(infoArea, BorderLayout.CENTER);
+		JLabel statusLabel = new JLabel("PASS", SwingConstants.CENTER);
+		statusLabel.setFont(new Font("Serif", Font.BOLD, 48));
+		statusLabel.setForeground(Color.GREEN);
+
+		JScrollPane scrollPane = new JScrollPane(infoArea);
+    panel.add(scrollPane, BorderLayout.CENTER);
+		panel.add(statusLabel, BorderLayout.SOUTH);
     panel.add(retArea, BorderLayout.NORTH);
 
     add(panel);  // add panel to frame
@@ -64,7 +78,7 @@ public class IptvView extends JFrame implements ViewInterface {
 
     // set Operation menu:
     JMenu operationMenu = new JMenu("Operation");
-    JMenuItem eraseMacItem = new JMenuItem("Erase Mac", 'E');
+    JMenuItem eraseMacItem = new JMenuItem("Erase Mac");
     JRadioButtonMenuItem restorationMode = 
       new JRadioButtonMenuItem("Restoration Mode");
     JMenuItem exitItem = new JMenuItem("Exit");
@@ -82,7 +96,7 @@ public class IptvView extends JFrame implements ViewInterface {
     
     //set Help menu
     JMenu helpMenu = new JMenu("Help");
-    JMenuItem aboutItem = new JMenuItem("About", 'A');
+    JMenuItem aboutItem = new JMenuItem("About");
     helpMenu.add(aboutItem);
 
     menuBar.add(operationMenu);
@@ -93,5 +107,73 @@ public class IptvView extends JFrame implements ViewInterface {
 
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
+		
+		inputDialog = new InputDialog(this, true);
+		inputDialog.setVisible(true);
   }
+
+	public void macWritingPerformed(MacWritingEvent e) {
+	}
+
+	public class InputDialog extends JDialog {
+
+		public InputDialog(Frame parent, boolean modal) {
+			super(parent, modal);
+
+			JPanel inputPanel = new JPanel();
+			inputPanel.setLayout(new GridLayout(2, 2, 0, 10));
+
+			JLabel snLabel = new JLabel("Input SN: ", SwingConstants.CENTER);
+			snField = new JTextField(40);
+			snField.setBorder(BorderFactory.createLineBorder(Color.RED, 5));
+
+			JLabel macLabel = new JLabel("Input mac: ", SwingConstants.CENTER);
+			macField = new JTextField(40);
+			
+			inputPanel.add(snLabel);
+			inputPanel.add(snField);
+			inputPanel.add(macLabel);
+			inputPanel.add(macField);
+			
+			JButton closeButton = new JButton("X");
+			closeButton.addActionListener(event -> {
+				this.setVisible(false);
+			});
+
+			this.setLayout(new BorderLayout());
+			this.add(inputPanel, BorderLayout.CENTER);
+			this.add(closeButton, BorderLayout.SOUTH);
+
+			this.pack();
+			
+			snField.addActionListener(new SnListener());
+			/*
+			macField.addActionListener(new MacListener());
+			*/
+		}
+
+		class SnListener implements ActionListener {
+			String sn;
+			String mac;
+
+			public void actionPerformed(ActionEvent e) {
+				sn = snField.getText().trim();
+
+				if (sn == null || sn.length() < 25) {
+					JOptionPane.showMessageDialog(InputDialog.this,
+							"Invalidate SN, please Check!",
+							"Wrong SN",
+							JOptionPane.WARNING_MESSAGE);
+					return;
+				}
+
+				macField.setFocusable(true);
+				macField.requestFocus(true);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		IptvView view = new IptvView();
+	}
 }
